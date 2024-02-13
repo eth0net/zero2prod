1a1654c76a24f4284f183f2516b5b585c784fa3e
Elliot Thomas <eth0net@pm.me>
2024-02-13 11:49:10 +0000

    db init

---
+++ scripts/init_db.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+# set -x
+set -eo pipefail
+
+if ! [ -x "$(command -v docker)" ]; then
+    echo >&2 "Error: docker is not installed."
+    exit 1
+fi
+
+if ! [ -x "$(command -v sqlx)" ]; then
+    echo >&2 "Error: sqlx is not installed."
+    echo >&2 "You can install it using:"
+    echo >&2 "    cargo install --version='~0.7' sqlx-cli --no-default-features --features rustls,postgres"
+    exit 1
+fi
+
+CONTAINER_NAME="zero2prod_db"
+
+DB_USER="${POSTGRES_USER:=postgres}"
+DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
+DB_NAME="${POSTGRES_DB:=newsletter}"
+DB_PORT="${POSTGRES_PORT:=5432}"
+DB_HOST="${POSTGRES_HOST:=localhost}"
+
+if docker ps -a --format '{{.Names}}' | grep -q "$CONTAINER_NAME"; then
+    echo "Database container $CONTAINER_NAME is already running"
+else
+    echo "Starting database container $CONTAINER_NAME"
+    docker run -d \
+        -e POSTGRES_USER=${DB_USER} \
+        -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+        -e POSTGRES_DB=${DB_NAME} \
+        -p "${DB_PORT}":5432 \
+        --name "${CONTAINER_NAME}" \
+        postgres \
+        postgres -N 1000
+fi
+
+until docker exec -it zero2prod_db psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+    >&2 echo "Postgres is still unavailable - sleeping"
+    sleep 1
+done
+
+>&2 echo "Postgres is up and running on port ${DB_PORT}!"
+
+export DATABASE_URL="postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}"
+
+sqlx database create
+sqlx migrate run
+
+>&2 echo "Postgres has been migrated, ready to go!"
