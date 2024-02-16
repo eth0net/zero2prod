4a3e44f7212f374772378bce57cf6737b8c1ee31
Elliot Thomas <eth0net@pm.me>
2024-02-16 16:18:53 +0000

    instrument subscribe handler

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use std::io::Result;
 use std::net::TcpListener;
 
+use env_logger::Env;
 use sqlx::PgPool;
 use zero2prod::config::get_config;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let config = get_config().expect("Failed to read configuration.");
 
     let address = format!("127.0.0.1:{}", config.application_port);
--- src/startup.rs
+++ src/startup.rs
@@ -2,6 +2,7 @@ use std::io::Result;
 use std::net::TcpListener;
 
 use actix_web::dev::Server;
+use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 
@@ -11,6 +12,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server> {
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health", web::get().to(health))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
